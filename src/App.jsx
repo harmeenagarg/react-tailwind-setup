02bb2debb5c1ae1faa534727899538cea6d675c9
Harmeena <harmeenagarg@gmail.com>
2025-04-08 01:24:30 +0530

    Fix Tailwind setup and add working hero layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 function App() {
   return (
-    <div>App</div>
-  )
+    <div className="min-h-screen bg-green-500 flex items-center justify-center">
+      <h1 className="text-white text-4xl">Hello There !</h1>
+    </div>
+  );
 }
 
 export default App
